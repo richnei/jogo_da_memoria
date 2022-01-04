b4c029b02e9b5a460f3e298d7e05b94468bf4c3d
Richnei <rich.nei@hotmail.com>
2022-01-04 20:26:25 -0300

    view

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card";
+const ICON = "icon";
 
 let emojis = [
   "abobora",
@@ -31,7 +33,41 @@ starGame();
 function starGame() {
   cards = createCardsFromEmojis(emojis);
   shuffleCards(cards);
-  console.log(cards);
+  initializeCards(cards);
+}
+
+function initializeCards(cards) {
+  let gameBoard = document.getElementById("gameBoard");
+  cards.forEach((card) => {
+    let cardElement = document.createElement("div");
+    cardElement.id = card.id;
+    cardElement.classList.add(CARD);
+    cardElement.dataset.icon = card.icon;
+
+    createCardContent(card, cardElement);
+
+    cardElement.addEventListener("click", flipCard);
+    gameBoard.appendChild(cardElement);
+  });
+}
+
+function createCardContent(card, cardElement) {
+  createCardFace(FRONT, card, cardElement);
+  createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+  let cardElementFace = document.createElement("div");
+  cardElementFace.classList.add(face);
+  if (face === FRONT) {
+    let iconElement = document.createElement("img");
+    iconElement.classList.add(ICON);
+    iconElement.src = "./assets/images/" + card.icon + ".png";
+    cardElementFace.appendChild(iconElement);
+  } else {
+    cardElementFace.innerHTML = "&lt/&gt";
+  }
+  element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards) {
@@ -53,9 +89,9 @@ createCardsFromEmojis(emojis);
 function createCardsFromEmojis(emojis) {
   let cards = [];
 
-  for (let emoji of emojis) {
+  emojis.forEach((emoji) => {
     cards.push(createPairFromEmoji(emoji));
-  }
+  });
 
   return cards.flatMap((pair) => pair);
 
@@ -77,4 +113,8 @@ function createCardsFromEmojis(emojis) {
   function createIdWithEmoji(emoji) {
     return emoji + parseInt(Math.random() * 1000);
   }
+}
+
+function flipCard() {
+  this.classList.add("flip");
 }
