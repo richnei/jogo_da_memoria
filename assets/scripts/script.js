797d3d4203a9c237265c5c42edeb03991a8645af
Richnei <rich.nei@hotmail.com>
2022-01-05 18:51:22 -0300

    ok

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -43,5 +43,19 @@ function createCardFace(face, card, element) {
   element.appendChild(cardElementFace);
 }
 function flipCard() {
-  this.classList.add("flip");
+  if (game.setCard(this.id)) {
+    this.classList.add("flip");
+    if (game.checkMatch()) {
+      game.clearCards();
+    } else {
+      setTimeout(() => {
+        let firstCardView = document.getElementById(game.firstCard.id);
+        let secondCardView = document.getElementById(game.secondCard.id);
+
+        firstCardView.classList.remove("flip");
+        secondCardView.classList.remove("flip");
+        game.clearCards();
+      }, 1000);
+    }
+  }
 }
