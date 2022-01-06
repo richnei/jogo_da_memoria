166756c7df718763cd62cb812fc98e4dd993d203
Richnei <rich.nei@hptmail.com>
2022-01-06 20:36:04 -0300

    bugs

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -4,7 +4,7 @@ let game = {
   secondCard: null,
 
   setCard: function (id) {
-    let card = this.cards.filter(card=>card.id===id)[0];
+    let card = this.cards.filter((card) => card.id === id)[0];
 
     if (card.flipped || this.lockMode) {
       return false;
@@ -12,15 +12,20 @@ let game = {
 
     if (!this.firstCard) {
       this.firstCard = card;
+      this.firstCard.flipped = true;
       return true;
     } else {
       this.secondCard = card;
+      this.secondCard.flipped = true;
       this.lockMode = true;
       return true;
     }
   },
 
   checkMatch: function () {
+    if (!this.firstCard || !this.secondCard) {
+      return false;
+    }
     return this.firstCard.icon === this.secondCard.icon;
   },
 
@@ -29,6 +34,17 @@ let game = {
     this.secondCard = null;
     this.lockMode = false;
   },
+
+  unflipCards(){
+    this.firstCard.flipped = false;
+    this.secondCard.flipped = false;
+    this.clearCards();
+  },
+
+  checkGameOver() {
+    return this.cards.filter((card) => !card.flipped).length == 0;
+  },
+
   emojis: [
     "abobora",
     "arvore_natal",
